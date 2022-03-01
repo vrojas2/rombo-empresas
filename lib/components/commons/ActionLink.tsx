2117862b8604533dbe7737617c0c5f15ad00103f
Uziel Medina <uzielmedina@hotmail.com>
2022-03-01 16:18:38 -0600

    Se cambiaron tamaños y colores

--- lib/components/commons/ActionLink.tsx
+++ lib/components/commons/ActionLink.tsx
@@ -1,4 +1,4 @@
-import { Button, Hidden, IconButton, Tooltip } from '@material-ui/core'
+import { Button, Hidden, IconButton, Tooltip, makeStyles } from '@material-ui/core'
 import Link from 'next/link'
 import { ReactElement } from 'react'
 
@@ -8,17 +8,23 @@ type ActionLinkProps = {
   icon: ReactElement<any, any>
   color?: 'inherit' | 'primary' | 'secondary' | 'default'
 }
+const useStyles = makeStyles({
+  buton: {
+    height: '200px',
+  },
+});
 export default function ActionLink({
   href,
   icon,
   label,
   color = 'default',
 }: ActionLinkProps): JSX.Element {
+  const classes = useStyles()
   return (
     <>
       <Hidden smDown>
         <Link href={href} passHref>
-          <Button color={color} fullWidth startIcon={icon}>
+          <Button color={color} fullWidth startIcon={icon} className={classes.buton}>
             {label}
           </Button>
         </Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -52,6 +52,8 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   indicador: {
     textAlign: 'center',
+    height:'300px',
+    background: 'linear-gradient(217deg, rgba(255,0,0,.8), rgba(255,0,0,0) 70.71%),linear-gradient(127deg, rgba(0,255,0,.8), rgba(0,255,0,0) 70.71%),linear-gradient(336deg, rgba(0,0,255,.8), rgba(0,0,255,0) 70.71%);'
   },
   indicadorLabel: {
     fontSize: theme.typography.h5.fontSize,
