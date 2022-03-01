3e0aa1a249da2465ce987de6259a0b1b5d41ba3f
Uziel Medina <uzielmedina@hotmail.com>
2022-03-01 16:29:11 -0600

    Cambio colores de texto y fondos

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,8 +52,11 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   indicador: {
     textAlign: 'center',
+    color:'white',
     height:'300px',
-    background: 'linear-gradient(217deg, rgba(255,0,0,.8), rgba(255,0,0,0) 70.71%),linear-gradient(127deg, rgba(0,255,0,.8), rgba(0,255,0,0) 70.71%),linear-gradient(336deg, rgba(0,0,255,.8), rgba(0,0,255,0) 70.71%);'
+    borderRadius: '50px',
+    background: 'linear-gradient(to bottom right, #8e2de2, #4a00e0);'
+
   },
   indicadorLabel: {
     fontSize: theme.typography.h5.fontSize,
